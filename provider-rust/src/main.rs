5efaa8636cd46a1a84ccdbba1eb77bc62567e90d
Reverb <j_kroeker@reverb256.ca>
2026-07-25 16:28:51 -0500

    fix: cargo fmt Box::pin argument formatting

--- provider-rust/src/main.rs
+++ provider-rust/src/main.rs
@@ -95,9 +95,12 @@ async fn search_yaml_files(base_dir: &str, key: &str, max_depth: u32) -> Option<
             continue;
         }
         if path.is_dir() {
-            if let Some(value) =
-                Box::pin(search_yaml_files(&path.to_string_lossy(), key, max_depth - 1))
-                    .await
+            if let Some(value) = Box::pin(search_yaml_files(
+                &path.to_string_lossy(),
+                key,
+                max_depth - 1,
+            ))
+            .await
             {
                 return Some(value);
             }
